9a3aec5d151bc1b0546d495a07381f1fc4f27c91
Sh0tlan <x359623@ukr.net>
2023-03-14 16:03:49 +0200

    first commit

---
+++ src/components/UI/Input.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import classes from "./Input.module.css";
+
+const Input = (props) => {
+  return (
+    <div className={classes.input}>
+      <label htmlFor={props.input.id} className>
+        {props.label}
+      </label>
+      <input {...props.input}></input>
+    </div>
+  );
+};
+
+export default Input;
